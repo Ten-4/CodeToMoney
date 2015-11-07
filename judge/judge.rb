e282bbe30d8a1d798729577805b2866e0ee31aaf
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2015-11-07 11:52:24 +0100

    Local testing code.

--- judge/judge.rb
+++ judge/judge.rb
@@ -1,7 +1,9 @@
 require 'tmpdir'
 require 'open3'
+require 'fileutils'
+require 'pathname'
 
-def judge(base_repo, solution_repo)
+def judge_git(base_repo, solution_repo)
   Dir.mktmpdir do |dir|
     puts "Tmp dir: #{dir}"
     Dir.chdir(dir) do
@@ -12,16 +14,35 @@ def judge(base_repo, solution_repo)
   end
 end
 
+def judge_local(tester, solution)
+  tester = Pathname(tester).realpath
+  solution = Pathname(solution).realpath
+  Dir.mktmpdir do |dir|
+    puts "Tmp dir: #{dir}"
+    Dir.chdir(dir) do
+      run_tests_py(tester, solution)
+    end
+  end
+end
+
 def clone_repo(repo, dest)
   run('git', 'clone', '--depth=1', repo, dest)
 end
 
-def run_tests_py(base_dir, sol_dir)
+def run_tests_git_py(base_dir, sol_dir)
   Dir.chdir(sol_dir) do
     out, err, status = Open3.capture3({"PYTHONPATH" => base_dir}, 'nosetests2', '-P')
     puts "Test output", err
     return status == 0, err
   end
+end
+
+def run_tests_py(tester, solution)
+  FileUtils.cp(solution, 'solution.py')
+  FileUtils.cp(tester, 'test_solution.py')
+  out, err, status = Open3.capture3('nosetests2', 'test_solution.py')
+  puts "Test output", err
+  return status == 0
 end
 
 def run(*args)
